87e9b17f5a15f5a185539fd054103a6300093576
Чуприков Станислав Андреевич <chupaw1990@gmail.com>
2024-05-21 16:52:46 +0300

    fix bug

--- function.h
+++ function.h
@@ -1,6 +1,11 @@
-#include <vector>
-#include <string>
+#pragma once
+
 #include <iostream>
+#include <string>
+#include <vector>
+
+using vector_ip_int = std::vector<std::vector<int>>;
+using vector_ip_str = std::vector<std::string>;
 
 // ("",  '.') -> [""]
 // ("11", '.') -> ["11"]
@@ -8,14 +13,13 @@
 // ("11.", '.') -> ["11", ""]
 // (".11", '.') -> ["", "11"]
 // ("11.22", '.') -> ["11", "22"]
-std::vector<std::string> split(const std::string &str, char d)
-{
+vector_ip_str split(const std::string &str, char d) {
     std::vector<std::string> r;
+    r.reserve(1000);
 
     std::string::size_type start = 0;
     std::string::size_type stop = str.find_first_of(d);
-    while(stop != std::string::npos)
-    {
+    while (stop != std::string::npos) {
         r.push_back(str.substr(start, stop - start));
 
         start = stop + 1;
@@ -27,29 +31,28 @@ std::vector<std::string> split(const std::string &str, char d)
     return r;
 }
 
-std::vector<std::vector<int>> ip_str_int(std::vector<std::vector<std::string>> &ip_pool_str) {
+vector_ip_int ip_str_int(std::vector<std::vector<std::string>> &ip_pool_str) {
+    vector_ip_int ip_pool_int;
+    ip_pool_int.reserve(1000);
+    std::vector<int> ip_int;
+    ip_int.reserve(1000);
 
-        using my_str = std::vector<std::string>;
-
-        std::vector<std::vector<int>> ip_pool_int;
-        std::vector<int> ip_int;
-
-        for (my_str i: ip_pool_str) {
-            ip_int.clear();
-            for(std::string j: i) {
-                ip_int.push_back(std::stoi(j));
-            }
-            ip_pool_int.push_back(ip_int);            
+    for (vector_ip_str i : ip_pool_str) {
+        ip_int.clear();
+        for (std::string j : i) {
+            ip_int.push_back(std::stoi(j));
         }
+        ip_pool_int.push_back(ip_int);
+    }
     return ip_pool_int;
 }
 
-std::ostream& operator << (std::ostream &os, std::vector<int> ip){
+std::ostream &operator<<(std::ostream &os, std::vector<int> ip) {
     std::string str = "";
     std::vector<int>::const_iterator ip_part = ip.cbegin();
-    for (int i : ip){
+    for (int i : ip) {
         str += std::to_string(i);
-        if (ip_part != ip.cend() - 1){
+        if (ip_part != ip.cend() - 1) {
             str += '.';
         }
         ip_part++;
@@ -57,32 +60,32 @@ std::ostream& operator << (std::ostream &os, std::vector<int> ip){
     return os << str << std::endl;
 }
 
-void print_ip (std::vector<std::vector<int>> &ip_pool_int){
-    for (std::vector<int> ip : ip_pool_int){
+void print_ip(vector_ip_int &ip_pool_int) {
+    for (auto ip : ip_pool_int) {
         std::cout << ip;
     }
 }
 
-void filter_ip(std::vector<std::vector<int>> &in_ip_pool, int first_byte) {
-    for(std::vector<int> ip : in_ip_pool){
-        if(ip[0] == first_byte){
+void filter_ip(vector_ip_int &in_ip_pool, int first_byte) {
+    for (auto ip : in_ip_pool) {
+        if (ip[0] == first_byte) {
             std::cout << ip;
         }
     }
 }
 
-void filter_ip(std::vector<std::vector<int>> &in_ip_pool, int first_byte, int second_byte) {
-    for(std::vector<int> ip : in_ip_pool){
-        if((ip[0] == first_byte) && (ip[1] == second_byte)){
+void filter_ip(vector_ip_int &in_ip_pool, int first_byte, int second_byte) {
+    for (auto ip : in_ip_pool) {
+        if ((ip[0] == first_byte) && (ip[1] == second_byte)) {
             std::cout << ip;
         }
     }
 }
 
-void filter_ip_any(std::vector<std::vector<int>> &in_ip_pool, int search_byte){
-    for(std::vector<int> ip : in_ip_pool){
-        if((ip[0] == search_byte) || (ip[1] == search_byte) || 
-           (ip[2] == search_byte) || (ip[3] == search_byte)) {
+void filter_ip_any(vector_ip_int &in_ip_pool, int search_byte) {
+    for (auto ip : in_ip_pool) {
+        if ((ip[0] == search_byte) || (ip[1] == search_byte) ||
+            (ip[2] == search_byte) || (ip[3] == search_byte)) {
             std::cout << ip;
         }
     }
--- test.cpp
+++ test.cpp
@@ -1,8 +1,10 @@
 #include <gtest/gtest.h>
-#include "function.h"
+
+#include <iostream>
 #include <string>
 #include <vector>
-#include <iostream>
+
+#include "function.h"
 
 TEST(Function, split_test) {
     std::vector<std::string> output_vec;
@@ -10,20 +12,20 @@ TEST(Function, split_test) {
 
     std::string input_str = "";
     char splitter = '.';
-    output_vec = split(input_str,splitter);
+    output_vec = split(input_str, splitter);
 
-    ASSERT_EQ (output_vec.size(), 1);
+    ASSERT_EQ(output_vec.size(), 1);
 
     output_vec.clear();
     input_str = "11";
     splitter = '.';
-    output_vec = split(input_str,splitter);
-    ASSERT_EQ (output_vec.size(), 1);
-    ASSERT_EQ (output_vec[0], "11");
+    output_vec = split(input_str, splitter);
+    ASSERT_EQ(output_vec.size(), 1);
+    ASSERT_EQ(output_vec[0], "11");
 
     output_vec.clear();
     input_str = "..";
     splitter = '.';
-    output_vec = split(input_str,splitter);
-    ASSERT_EQ (output_vec.size(), 3);
+    output_vec = split(input_str, splitter);
+    ASSERT_EQ(output_vec.size(), 3);
 }
